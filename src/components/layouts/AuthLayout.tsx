60ff5f30fe2e80d6b01965d222442bb82b5954c9
Hilaladiii <abdulhilal242@gmail.com>
2025-01-16 18:44:45 +0700

    feat : add auth layout

---
+++ src/components/layouts/AuthLayout.tsx
@@ -0,0 +1,17 @@
+export default function AuthLayout({
+  title,
+  description,
+  children,
+}: {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="w-full max-w-lg">
+      <h1 className="text-4xl font-semibold">{title}</h1>
+      <p className="mt-4 text-base text-darkGrey">{description}</p>
+      {children}
+    </div>
+  );
+}
